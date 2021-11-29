91e4b84a191678c7460c435c84e7f24bc2bf7608
Santiago Yanguas <santitoya-2001@outlook.com>
2021-11-29 17:19:56 -0500

    [Fix] Package outside of the folder types css

--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -1,59 +1,46 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, NavLink, Navigate } from "react-router-dom";
-import logo from "../logo.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
 
-interface ActiveRouter {
-  isActive: boolean;
-}
+import logo from '../logo.svg';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="main-layout">
         <nav>
-          <img src={logo} alt="logo" />
+            <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink
-                to="/lazy1"
-                className={({ isActive }: ActiveRouter): string =>
-                  isActive ? "nav-active" : ""
-                }
-              >
-                Lazy1
-              </NavLink>
+              <NavLink to="/" activeClassName="nav-active" exact>Shopping</NavLink>
             </li>
             <li>
-              <NavLink
-                to="/lazy2"
-                className={({ isActive }: ActiveRouter): string =>
-                  isActive ? "nav-active" : ""
-                }
-              >
-                Lazy2
-              </NavLink>
+              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
             </li>
             <li>
-              <NavLink
-                to="/lazy3"
-                className={({ isActive }: ActiveRouter): string =>
-                  isActive ? "nav-active" : ""
-                }
-              >
-                Lazy3
-              </NavLink>
+              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
             </li>
           </ul>
         </nav>
 
-        <Routes>
-          <Route path="lazy1" element={<LazyPage1 />} />
-          <Route path="lazy2" element={<LazyPage2 />} />
-          <Route path="lazy3" element={<LazyPage3 />} />
-          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
-        </Routes>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+          <Route path="/">
+            <ShoppingPage />
+          </Route>
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   );
-};
+}
